7495c4d84556b4a828576e22d788f756c3832f3f
John <johnfoderaro@icloud.com>
2017-10-05 22:26:41 -0400

    updating handler module

--- test/handler.js
+++ test/handler.js
@@ -2,12 +2,10 @@ const assert = require('assert');
 const handler = require('./../lib/handler');
 
 describe('handler', () => {
-  it('should return null', () => {
-    const request = handler();
-    assert.equal(request(), false);
-  });
   it('should return an object', () => {
-    const request = handler({});
-    assert.ok(request() instanceof Object);
+    const request01 = handler();
+    const request02 = handler({});
+    assert.ok(request01() instanceof Object);
+    assert.ok(request02() instanceof Object);
   });
 });
