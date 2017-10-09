41efe0d29217b0aea0a121e8e58c186ab1ed88df
John Foderaro <johnfoderaro@icloud.com>
2017-10-09 09:35:49 -0400

    updating lib

--- test/webhook.js
+++ test/webhook.js
@@ -1,33 +1,29 @@
 const assert = require('assert');
 
-const WebHook = require('./../lib/webhook');
+const webhook = require('./../lib/webhook');
 const post = require('./post');
 
-describe('WebHook', () => {
-  it('should return an instance of WebHook', async () => {
-    const project = WebHook({ port: 3001, endPoint: '/build/' });
-    post({ port: 3001, endPoint: '/build/', data: { test: true } });    
-    const data = await project.stream();
-    console.log(data);
-    // assert.ok(project instanceof Object);
+describe('webhook', () => {
+  it('webhook() should return an instance of an Object', () => {
+    const project = webhook({
+      port: 3001,
+      endPoint: '/build/',
+      token: 123456,
+      secret: true,
+    });
+    assert.ok(project instanceof Object);
+    project.server().close();
   });
-  // it('stream() should return an object', async () => {
-  //   const project = WebHook({ port: 3002, endPoint: '/build/' });
-  //   post({ port: 3002, endPoint: '/build/', data: { test: true } });    
-    // project.stream((data) => {
-    //   assert.ok(data instanceof Object);
-    //   return this.server.close();
-    // });
-    
-    // try {
-    //   console.log(project.stream());
-    // } catch (error) {
-    //   console.error(error);
-    // }
-    // project.stream().then(data => console.log(data));
-  // });
-  // it('commands() should return an object', () => {
-  //   const project = WebHook({ port: 3003, endPoint: '/build/' });
-  //   project.commands(repo => console.log(`git clone ${repo}`));
-  // });
+  it('webhook().listen() should return an instance of an Object', () => {
+    const project = webhook({
+      port: 3001,
+      endPoint: '/build/',
+      token: 123456,
+      secret: true,
+    });
+    project.listen().then((payload) => {
+      assert.ok(payload instanceof Object)
+    }).catch(err => console.error(err));
+    post({ port: 3001, endPoint: '/build/', data: { test: true } });
+  });
 });
