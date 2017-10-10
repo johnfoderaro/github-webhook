3274bbc16b1dbd630042a7ea0ebbb33f33dcc271
John Foderaro <johnfoderaro@icloud.com>
2017-10-09 22:43:46 -0400

    updating class

--- test/webhook.js
+++ test/webhook.js
@@ -4,26 +4,41 @@ const webhook = require('./../lib/webhook');
 const post = require('./post');
 
 describe('webhook', () => {
-  it('webhook() should return an instance of an Object', () => {
+  it('webhook should return an instance of an Object', () => {
     const project = webhook({
       port: 3001,
       endPoint: '/build/',
       token: 123456,
       secret: true,
+      response: 'Payload received. Check logs for details.',
     });
     assert.ok(project instanceof Object);
     project.server().close();
   });
-  it('webhook().listen() should return an instance of an Object', () => {
+  it('listen() should resolve promose with instance of Object', () => {
     const project = webhook({
       port: 3001,
       endPoint: '/build/',
       token: 123456,
       secret: true,
+      response: 'Payload received. Check logs for details.',
     });
-    project.listen().then((payload) => {
-      assert.ok(payload instanceof Object)
-    }).catch(err => console.error(err));
     post({ port: 3001, endPoint: '/build/', data: { test: true } });
+    return project.listen().then((payload) => {
+      assert.ok(payload instanceof String);
+      project.server().close();
+    }).catch(err => console.error(err));
   });
+  // it('spawn() should resolve promise with instance of Object', () => {
+  //   const project = webhook({
+  //     port: 3001,
+  //     endPoint: '/build/',
+  //     token: 123456,
+  //     secret: true,
+  //     response: 'Payload received. Check logs for details.',
+  //   });
+  //   return project.listen().then((payload) => {
+  //     assert.ok(payload instanceof Object);
+  //   }).catch(err => console.error(err));
+  // });
 });
