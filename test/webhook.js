366bb884778306232d300d0b806823e4a1f8276a
John Foderaro <johnfoderaro@icloud.com>
2017-10-10 23:56:14 -0400

    passing tests

--- test/webhook.js
+++ test/webhook.js
@@ -13,7 +13,6 @@ describe('webhook', () => {
       response: 'Payload received. Check logs for details.',
     });
     assert.ok(project instanceof Object);
-    project.server().close();
   });
   it('listen() should resolve promose with instance of Object', () => {
     const project = webhook({
@@ -25,20 +24,29 @@ describe('webhook', () => {
     });
     post({ port: 3001, endPoint: '/build/', data: { test: true } });
     return project.listen().then((payload) => {
-      assert.ok(payload instanceof String);
-      project.server().close();
+      project.server.close();
+      return assert.ok(payload instanceof Object);
     }).catch(err => console.error(err));
   });
-  // it('spawn() should resolve promise with instance of Object', () => {
-  //   const project = webhook({
-  //     port: 3001,
-  //     endPoint: '/build/',
-  //     token: 123456,
-  //     secret: true,
-  //     response: 'Payload received. Check logs for details.',
-  //   });
-  //   return project.listen().then((payload) => {
-  //     assert.ok(payload instanceof Object);
-  //   }).catch(err => console.error(err));
-  // });
+  it('spawn() should resolve promise with instance of Object', () => {
+    const project = webhook({
+      port: 3001,
+      endPoint: '/build/',
+      token: 123456,
+      secret: true,
+      response: 'Payload received. Check logs for details.',
+    });
+    const commands = payload => [{
+      command: 'git',
+      args: ['clone', `https://${project.token}@github.com/${payload.repo}`],
+      options: {},
+    }];
+    post({ port: 3001, endPoint: '/build/', data: { test: true } });
+    return project.listen().then((payload) => {
+      project.server.close();
+      project.spawn(commands(payload))
+        .then(stdout => assert.ok(stdout))
+        .catch(stderr => project.log(stderr));
+    });
+  });
 });
