236e2f1391ef582ff68b246f440a21079a780e6d
John <johnfoderaro@icloud.com>
2017-10-25 00:14:37 -0400

    hmac digest for github sha1 verification

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-// const WebHook = require('./lib/webhook');
+const WebHook = require('./lib/webhook');
 
-// const project01 = WebHook({ port: 3004, endPoint: '/build/' });
-// project01.stream(data => console.log(data));
+const project = WebHook({
+  port: 3000,
+  endPoint: '/build/',
+  token: '123456',
+  secret: '123456',
+  response: 'Payload received. Check logs for details.',
+});
+
+project.listen().then((payload) => {
+  // console.log(payload);
+}).catch(err => console.error(err));
